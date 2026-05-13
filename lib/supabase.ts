687712f44f1c38948fbe7efbb64a6b58661d31b7
Shubhang Arora <shubhangaroraa@gmail.com>
2026-05-14 02:31:19 +0530

    fix prerender error

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,8 +1,12 @@
 import { createClient as createSupabaseClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-
 export function createClient() {
+  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
+  const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+
+  if (!supabaseUrl || !supabaseKey) {
+    throw new Error('Missing Supabase environment variables')
+  }
+
   return createSupabaseClient(supabaseUrl, supabaseKey)
 }
