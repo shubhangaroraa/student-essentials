8c8d46f2d27ff4e95abc71bdebe8d58761746f66
Shubhang Arora <shubhangaroraa@gmail.com>
2026-05-15 00:38:53 +0530

    disable middleware temporarily

--- middleware.ts
+++ middleware.ts
@@ -1,19 +1,5 @@
-import { NextResponse, type NextRequest } from 'next/server'
+import { NextResponse } from 'next/server'
 
-export async function middleware(request: NextRequest) {
-  if (request.nextUrl.pathname.startsWith('/dashboard')) {
-    const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
-    const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!
-    
-    const token = request.cookies.get('sb-ymsxsqqopbwuuctvokg-auth-token')
-    
-    if (!token) {
-      return NextResponse.redirect(new URL('/auth/login', request.url))
-    }
-  }
+export function middleware() {
   return NextResponse.next()
-}
-
-export const config = {
-  matcher: ['/dashboard/:path*'],
 }
